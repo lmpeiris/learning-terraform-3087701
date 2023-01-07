39efbfbb6c74b2208e564567ddef985428a5c6e1
Malshan Peiris <lmpeiris@gmail.com>
2023-01-07 21:35:53 +0530

    change sg to use terraform module

--- main.tf
+++ main.tf
@@ -51,11 +51,25 @@ resource "aws_security_group_rule" "default_sg_outgoing" {
   security_group_id = aws_security_group.default_sg.id
 }
 
+module "sg-module" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.16.2"
+  name = "terra-tute-sg-mod"
+  
+  vpc_id = data.aws_vpc.default.id
+  
+  # module feature - named rules
+  ingress_rules = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
-  vpc_security_group_ids = [aws_security_group.default_sg.id]
-
+  # vpc_security_group_ids = [aws_security_group.default_sg.id]
+  vpc_security_group_ids = [module.sg-module.security_group_id]
   tags = {
     Name = "HelloWorld"
   }
