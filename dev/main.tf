6a4062b6cb0718d750c958d3d0619d3dda44bbb8
Malshan Peiris <lmpeiris@gmail.com>
2023-01-09 22:39:04 +0530

    fix typo

--- dev/main.tf
+++ dev/main.tf
@@ -1,7 +1,7 @@
 module "tomcat_ha" {
-    source "../modules/terra"
+    source        = "../modules/terra"
     instance_type = "t3a.nano"
-    min_size = 1
-    max_size = 1
-    public_port = 80
+    min_size      = 1
+    max_size      = 1
+    public_port   = 80
 }
