b87b27d7401334e961c85b4c7cf03aae0ac0469e
Malshan Peiris <lmpeiris@gmail.com>
2023-01-09 16:50:49 +0530

    added alb

--- main.tf
+++ main.tf
@@ -94,3 +94,43 @@ resource "aws_instance" "blog" {
     Name = "HelloWorld"
   }
 }
+
+module "terra-alb" {
+  source  = "terraform-aws-modules/alb/aws"
+  version = "8.2.1"
+
+  name = "terra-alb"
+
+  load_balancer_type = "application"
+
+  vpc_id             = module.terra-tute-vpc.vpc-vpc_id
+  subnets            = module.terra-tute-vpc.public_subnets
+  security_groups    = [module.sg-module.security_group_id]
+
+  target_groups = [
+    {
+      name_prefix      = "terra-alb-"
+      backend_protocol = "HTTP"
+      backend_port     = 80
+      target_type      = "instance"
+      targets = {
+        my_target = {
+          target_id = aws_instance.blog.id
+          port = 80
+        }
+      }
+    }
+  ]
+
+  http_tcp_listeners = [
+    {
+      port               = 80
+      protocol           = "HTTP"
+      target_group_index = 0
+    }
+  ]
+
+  tags = {
+    Environment = "dev"
+  }
+}
